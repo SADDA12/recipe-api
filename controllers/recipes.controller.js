2fef30692aa4efe20c2b0cf862e2428e55044373
SADDA12 <sammiedaga@gmail.com>
2024-03-25 16:51:58 +0000

    working on recipe app

--- controllers/recipes.controller.js
+++ controllers/recipes.controller.js
@@ -1,58 +1,57 @@
 import { RecipeModel } from "../models/recipe.js";
 
 export const addRecipe = async (req, res, next) => {
-   try {
+  try {
     // Add recipe to the data base
-    const createresult = await RecipeModel.create(req.body)
+    console.log(req.file);
+    const createResult = await RecipeModel.create({
+      ...req.body,
+      image: req.file.filename,
+    });
     // return response
-    res.json(createresult);
-    
-   } catch (error) {
+    res.status(201).json(createResult);
+  } catch (error) {
     // Forward to express error handler
     next(error);
-   }
-    
+  }
+};
 
-}
-
-export const getRecipes = async(req, res, next) => {
-    try {
-        // Get all recipes from database
-        const findRecipes = await RecipeModel.find()
-        // Return response
-        res.status(200).json(findRecipes)
-    } catch (error) {
-        // Forward to express error handler
-        next(error)
-        
-    }
-
-}
+export const getRecipes = async (req, res, next) => {
+  try {
+    // Get all recipes from database
+    const findRecipes = await RecipeModel.find();
+    // Return response
+    res.status(200).json(findRecipes);
+  } catch (error) {
+    // Forward to express error handler
+    next(error);
+  }
+};
 
 export const getRecipe = async (req, res, next) => {
-    try {
-        // Get single recipe with id
-        const recipeId = req.params.id
-        const findRecipe = await RecipeModel.findById(recipeId)
-        console.log(recipeId)
-        // Return 404 is recipe not found
-        if (findRecipe === null) {
-            return res.status(404).json({
-                message: `Recipe with ObjectId: ${req.params.id} Not Found!`
-            });
-        }
-        // Return response
-        res.status(200).json(findRecipe);
-    } catch (error) {
-        // Forward to express error handler
-        next(error);
+  try {
+    // Get single recipe with id
+    const recipeId = req.params.id;
+    const findRecipe = await RecipeModel.findById(recipeId);
+    console.log(recipeId);
+    // Return 404 is recipe not found
+    if (findRecipe === null) {
+      return res.status(404).json({
+        message: `Recipe with ObjectId: ${req.params.id} Not Found!`,
+      });
     }
-}
+    // Return response
+    res.status(200).json(findRecipe);
+  } catch (error) {
+    // Forward to express error handler
+    next(error);
+  }
+};
 
 export const updateRecpe = (req, res) => {
-    res.send(recipes);
-}
+  res.send(recipes);
+};
 
 export const deleteRecipe = (req, res) => {
-    res.send(recipes);
-}
+  res.send(recipes);
+};
--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { recipes } from "../data.js";
 import dotenv from "dotenv";
+import multer from "multer";
 import { addRecipe, deleteRecipe, getRecipe, getRecipes, updateRecpe } from "../controllers/recipes.controller.js";
 
+// Configure upload middleware
+const upload = multer({dest: 'uploads'});
+
+// Create recipes router
 export const recipesRouter = Router()
 
-recipesRouter.post('/', addRecipe)
+recipesRouter.post('/', upload.single('image'), addRecipe)
 
 recipesRouter.get('/', getRecipes)
 
